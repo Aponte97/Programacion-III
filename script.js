a7c05259fd53f67b6d0c9d6fdf8f810b710320de
Diego Aponte <diegaponte@gmail.com>
2024-10-06 13:12:13 -0500

    complementacion para que los botones muestren las tablas con la informacion del plan de estudio

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("botonDesplegarTabla1").addEventListener("click", function() {  /* esto es para las tablas*/
+    const contenedorTabla1 = document.getElementById("contenedorTabla1");
+    contenedorTabla1.classList.toggle("mostrar");
+});
+
+document.getElementById("botonDesplegarTabla2").addEventListener("click", function() {
+    const contenedorTabla2 = document.getElementById("contenedorTabla2");
+    contenedorTabla2.classList.toggle("mostrar");
+});
+
+document.getElementById("botonDesplegarTabla3").addEventListener("click", function() {
+    const contenedorTabla3 = document.getElementById("contenedorTabla3");
+    contenedorTabla3.classList.toggle("mostrar");
+});
+
+document.getElementById("botonDesplegarTabla4").addEventListener("click", function() {
+    const contenedorTabla4 = document.getElementById("contenedorTabla4");
+    contenedorTabla4.classList.toggle("mostrar");
+});
+
+document.getElementById("botonDesplegarTabla5").addEventListener("click", function() {
+    const contenedorTabla5 = document.getElementById("contenedorTabla5");
+    contenedorTabla5.classList.toggle("mostrar");
+});
+
+document.getElementById("botonDesplegarTabla6").addEventListener("click", function() {
+    const contenedorTabla5 = document.getElementById("contenedorTabla6");
+    contenedorTabla5.classList.toggle("mostrar");
+});
