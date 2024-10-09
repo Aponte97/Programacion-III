ff47fab02daa307c99c2f99fbf1cab1a07e87e78
juanes017 <juan.acevedo12@uptc.edu.co>
2024-10-09 12:34:22 -0500

    Validacion de formulario en js

--- script.js
+++ script.js
@@ -27,3 +27,35 @@ document.getElementById("botonDesplegarTabla6").addEventListener("click", functi
     const contenedorTabla5 = document.getElementById("contenedorTabla6");
     contenedorTabla5.classList.toggle("mostrar");
 });
+
+function formulario(nombreGrupo) {
+    document.getElementById('nombre-grupo').innerText = nombreGrupo;
+    
+    const formularioSection = document.getElementById('formulario');
+    formularioSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function validarFormulario() {
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const telefono = document.getElementById('telefono').value;
+
+    if (!nombre || !email || !telefono) {
+        alert("Por favor, complete todos los campos.");
+        return false;
+    }
+
+    const telefonoValidar = /^[0-9]+$/; 
+    if (!telefonoValidar.test(telefono)) {
+        alert("El número de teléfono solo debe contener dígitos.");
+        return false;
+    }
+
+    alert("Formulario enviado correctamente.");
+    return true;
+}
+
+function limpiarFormulario() {
+    document.getElementById('inscripcionForm').reset(); 
+
+}
